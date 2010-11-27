6a516a64f305accdf658301f73f9c0ca9c06f776
PC <pc@matt-schwarz.com>
2010-11-27 19:02:14 +0100

    added an example howto extend ApplicationController

--- lib/acts_as_widget/base.rb
+++ lib/acts_as_widget/base.rb
@@ -1,6 +1,7 @@
 module Cheese
   module ActsAsWidget
 
+    ## Define ModelMethods
     module Base
       def self.included(klass)
         klass.class_eval do
@@ -28,7 +29,42 @@ module Cheese
                 
       end # InstanceMethods      
     end
+
+
+    ## Define ControllerMethods
+    module Controller
+      def self.included(klass)
+        klass.class_eval do
+          self.send :extend, Cheese::ActsAsWidget::Controller::ClassMethods
+        end
+      end
+
+      module ClassMethods
+        ## this has to be wrapped inside a method or else it'll bark on "undefined method `before_filter'"
+        ## still trying to understand in which order which files need to be required to work around this
+        ## (since 2 frakin' days already...)
+        ## alternatively one can put the before_filter call into the real application_controller, which
+        ## removes the need for a separate acts_as_... helper
+        def acts_as_widget_controller
+          self.send :include, Cheese::ActsAsWidget::Controller::InstanceMethods
+          before_filter :test_controller_instance_method
+        end
+      end
+
+
+      module InstanceMethods
+        def test_controller_instance_method
+          puts "Calling an example application_controller instance_method"
+        end
+      end
+
+    end
+
   end
+
 end
 
 ::ActiveRecord::Base.send :include, Cheese::ActsAsWidget::Base
+
+## this one includes the given module into ActionController 
+::ActionController::Base.send :include, Cheese::ActsAsWidget::Controller
